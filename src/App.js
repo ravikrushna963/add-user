aab733b389a2ad07eede2597a8b8fdcf042b25e2
ravikrushna963 <ravikrushna963@gmail.com>
2022-10-07 17:50:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [errMsgName, setErrMsgName] = useState("");
+  const [errMsgEmail, setErrMsgEmail] = useState("");
+  const [errStatusName, setErrStatusName] = useState(false);
+  const [errStatusEmail, setErrStatusEmail] = useState(false);
+  const [result, setResult] = useState("");
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+    if (e.target.value === "") {
+      setErrMsgName("*Required");
+      setErrStatusName(true);
+    } else {
+      setErrMsgName("");
+      setErrStatusName(false);
+    }
+  };
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+    if (e.target.value === "") {
+      setErrMsgEmail("*Required");
+      setErrStatusEmail(true);
+    } else {
+      setErrMsgEmail("");
+      setErrStatusEmail(false);
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (name === "") {
+      setErrMsgName("*Required");
+      setErrStatusName(true);
+    } else {
+      setErrMsgName("");
+      setErrStatusName(false);
+    }
+    if (email === "") {
+      setErrMsgEmail("*Required");
+      setErrStatusEmail(true);
+    } else {
+      setErrMsgEmail("");
+      setErrStatusEmail(false);
+    }
+    if (email !== "" && name !== "") {
+      setResult("User added successfully..!!");
+    } else {
+      setResult();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-5">
+      <h1 className="heading mb-5">Add User</h1>
+      <form id="addUserForm" className="form" onSubmit={onSubmit}>
+        <div className="mb-3">
+          <label for="name" className="label">
+            Name
+          </label>
+          <br />
+          <input
+            type="text"
+            id="name"
+            className="text"
+            onChange={onChangeName}
+            value={name}
+          />
+          <br />
+          <p className="para" id="nameErrMsg">
+            {errStatusName && errMsgName}
+          </p>
+        </div>
+        <div className="mb-3">
+          <label for="email" className="label">
+            Email
+          </label>
+          <br />
+          <input
+            type="text"
+            id="email"
+            className="text"
+            onChange={onChangeEmail}
+            value={email}
+          />
+          <br />
+          <p className="para" id="emailErrMsg">
+            {errStatusEmail && errMsgEmail}
+          </p>
+        </div>
+        <button className="btn btn-primary mt-2 button">Submit</button>
+        <p className="result">{result}</p>
+      </form>
     </div>
   );
 }
 
 export default App;
+
